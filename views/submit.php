01b21aaf9881765af69ab0174b0c18623ecef04d
lily healey <lily@lilyhealey.co.uk>
2016-06-15 11:05:36 -0400

    update form

--- views/submit.php
+++ views/submit.php
@@ -1,4 +1,30 @@
 <?
+function is_valid_email($email)
+{
+	// return preg_match("/^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/i", $email);
+	return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+function send_mail($from, $to, $subject, $message)
+{
+	// address headers
+	$header = "From: $from <$from>\r\n";
+	$header.= "X-X-Sender: $from\r\n";
+	
+	// mail program headers
+	$header.= "Message-ID: <".md5(uniqid(time()))."@{$_SERVER['SERVER_NAME']}>\r\n";
+	
+    mail($to, $subject, $message, $header, "-f$from");
+}
+
+function send_mail_long($from_name, $from_email, $to_name, $to_email, $subject, $message)
+{
+	$header = "From: $from_name <$from_email>\r\n";
+	$header.= "Message-ID: <".md5(uniqid(time()))."@{$_SERVER['SERVER_NAME']}>\r\n";
+	$to = "$to_name <$to_email>";
+	mail($to, $subject, $message, $header, "-f$from_email");
+}
+
 if($_POST['action'] != "mail")
 {
 ?>
@@ -9,7 +35,7 @@ if($_POST['action'] != "mail")
             <td>Name</td>
             <td><input type="text" name="name"></td>
         <tr>
-            <td>Email</td> 
+            <td>Email</td>
             <td><input type="text" name="email"></td>
         </tr>
         <tr>
@@ -26,5 +52,14 @@ else
 {
     foreach($_POST as $k => $v)
         $$k = $v;
+
+    $from_name = $name;
+    $from_email = $email;
+    $subject = "Claim from ".$from_name;
+    $to_name = "lily";
+    $to_email = "lilyhealey1@gmail.com";
     
+    send_mail_long($from_name, $from_email, $to_name, $to_email, $subject, $body);
+    
+    ?>thank you!<?
 }

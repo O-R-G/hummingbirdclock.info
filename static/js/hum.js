e0ab7760f8d4d2468dcf25cdf42a8d5503307f11
David Reinfurt <reinfurt@o-r-g.com>
2016-07-05 00:32:33 -0400

    consolidate controls.js and hum.js

--- static/js/hum.js
+++ static/js/hum.js
@@ -7,6 +7,8 @@ var hum_max = 400;
 var vco;
 var vca;
 
+/* audio */
+
 function get_audio_context() {
     var this_audio_context = window.AudioContext || // default
                              window.webkitAudioContext || // safari
@@ -34,31 +36,46 @@ function init_hum() {
     // hum_timer = window.setInterval(change_frequency, 100);
 }
 
+/* controls */
 
+function init_controls() {
+	var audio = getCookie("audio");
+    var controls = document.getElementById("controls");
+    controls.addEventListener("click", hum_on_off);
+	if (audio != "off")
+        hum_on(controls);
+	else
+        hum_off(controls);
+}
 
+function hum_on_off() {
+	if (!audio_context) {
+		// if iOS need user event to unlock context
+	    get_audio_context();
+		init_hum();
+    	console.log(audio_context);
+	}
+    var controls = document.getElementById("controls");
+	var audio = getCookie("audio");
+	if (audio == "off")
+        hum_on(controls);
+	else
+        hum_off(controls);
+}
 
+function hum_on(controls) {
+    vca.gain.value = 1;
+    controls.innerHTML = "&times;";
+    set_cookie("audio", "on");
+    console.log("audio = " + getCookie("audio"));
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* this is left over for now as the frequency change is in clock.js */
+function hum_off(controls) {
+    vca.gain.value = 0;
+    controls.innerHTML = "+";
+    set_cookie("audio", "off");
+    console.log("audio = " + getCookie("audio"));
+}
 
 /*
 function change_frequency()
@@ -73,3 +90,28 @@ function change_frequency()
 }
 */
 
+/* cookies */
+
+function set_cookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ')
+            c = c.substring(1);
+        if (c.indexOf(name) == 0)
+            return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+function checkCookie(cname) {                
+    if (getCookie(cname) != "")
+        return true;
+    else
+        return false;
+}

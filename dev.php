8c0729fc4bc2ad5e67385d37cae65bd816de6f0f
David Reinfurt <reinfurt@o-r-g.com>
2016-07-07 05:28:01 -0400

    dev.php added for ios audio test

---
+++ dev.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<style>
+.button {
+    background-color: #000;
+    border: none;
+    color: #FFF;
+    padding: 15px 30px;
+    margin: 15px;
+    text-align: center;
+    font-size: 16px;
+}
+</style>
+<input id="play" class="button" type="button" value="* p l a y *"/>
+<input id="stop" class="button" type="button" value="* s t o p *">
+
+<script>
+
+// 0. create audio context
+// 1. create oscillator, define
+// 2. connect oscillator to gain
+// 3. create gain
+// 4. connect gain to output
+// 5. start oscillator (on click)
+
+var this_audio_context = window.AudioContext || // default
+                         window.webkitAudioContext || // safari
+                         false;
+audio_context = new this_audio_context;
+
+function init() {
+    var hum_base = 200;
+
+    // oscillator
+    vco = audio_context.createOscillator();
+    vco.type = 'sine';
+    vco.frequency.value = hum_base;
+
+    // gain
+    vca = audio_context.createGain();
+    vca.gain.value = .25;
+
+    // connect
+    vco.connect(vca);
+    vca.connect(audio_context.destination);
+}
+
+// listeners
+
+document.getElementById('play').addEventListener('click', on);
+document.getElementById('stop').addEventListener('click', off);
+
+// control
+
+function on() {
+    init();
+    vco.start(0); 
+    document.getElementById('play').style.color="#666";
+    document.getElementById('stop').style.color="#FFF";
+}
+
+function off() {
+    vco.stop(0); 
+    document.getElementById('play').style.color="#FFF";
+    document.getElementById('stop').style.color="#666";
+}
+
+function cleanup() {
+    vco.disconnect(0); 
+}
+</script>
+</html>
